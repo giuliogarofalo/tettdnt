4f4bd09419c47fe4eda6c8cfdb249f734eaea8f9
Giulio Garofalo <giuliogarofalo91@gmail.com>
2026-02-02 13:10:28 +0100

    replace js to jsx

--- test1/vite.config.js
+++ test1/vite.config.js
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /\.js$/,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
+  resolve: {
+    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
   },
 });
