f3b9ebe561d397c26b836fe5c2570d1fa3994eaa
Giulio Garofalo <giuliogarofalo91@gmail.com>
2026-02-02 13:13:59 +0100

    15

--- test1/app.jsx
+++ test1/app.jsx
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <Context.Provider value={{ addOrder }}>
       <CryptoBuy />
-      <Orders />
+      <Orders orders={orders} />
     </Context.Provider>
   );
 }
